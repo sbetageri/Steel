c89601a27f087db43c1542853fb5297c809df225
Srirakshith Betageri <sbetageri111@gmail.com>
2019-09-14 19:11:08 -0500

    Use Decoder Encoder files.

--- BaseUNet.py
+++ BaseUNet.py
@@ -4,10 +4,11 @@ from BaseUNetEncoder import UNetEncoder
 from BaseUNetDecoder import UNetDecoder
 
 class UNet(tf.keras.Model):
-    super(UNet, self).__init__()
+    def __init__(self):
+        super(UNet, self).__init__()
     
-    self.encoder = UNetEncoder()
-    self.decoder = UNetDecoder()
+        self.encoder = UNetEncoder()
+        self.decoder = UNetDecoder()
 
     def call(self, x):
         x, enc1, enc2, enc3, enc4 = self.encoder(x)
