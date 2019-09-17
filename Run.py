6e6ccebd111324e360efbccc8f603c14c89a729d
Srirakshith Betageri <sbetageri111@gmail.com>
2019-09-17 17:15:44 -0500

    Change loss and metrics.

--- Run.py
+++ Run.py
@@ -12,17 +12,19 @@ if __name__ == '__main__':
     img_dataset = img_dataset.batch(2)
     
     model = UNet()
-    loss_obj = tf.keras.losses.BinaryCrossentropy()
+    loss_obj = tf.keras.losses.MeanSquaredError()
+    iou_metric = tf.keras.metrics.MeanIoU(num_classes=4)
     optimizer = tf.keras.optimizers.Adam(learning_rate=1e-7)
     early_stop_cb = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
     tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir='./log_dir/', histogram_freq=1)
     
     model.compile(optimizer=optimizer,
-              loss = dataset.bce_dice_loss,
+              loss = loss_obj,
               metrics=[dataset.dice])
     
     history = model.fit(img_dataset,
                         epochs=10,
+                        metrics=[iou_metric]
                         callbacks=[early_stop_cb, tensorboard_cb])
     
     model.save('./model_weights', save_format='tf')
